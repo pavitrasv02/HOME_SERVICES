6f44b2b093cd172d6288ba03361ca584f6c3bdea
pavitra_sv <pramitharathod@gmail.com>
2026-04-22 21:05:00 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from .views import create_admin
+
+urlpatterns = [
+    path('create-admin/', create_admin),
+]
 
 urlpatterns = [
     path('', views.home, name='home'),
